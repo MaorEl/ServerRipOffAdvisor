fd18fd268ab9f56fde881369c81bfa6b1455f2c2
AnaelGor <anaellegorfinkel@gmail.com>
2019-05-14 17:16:40 +0300

    POST- add user works GET- get all users works

--- app.js
+++ app.js
@@ -1,53 +1,48 @@
 var express = require('express');
 var app = express();
+const DButilsAzure = require('./DButils');
+app.use(express.json()); //hels me read the JSON
 
-app.get('/', function (req, res) {
 
-    var sql = require("mssql");
+app.get('/all_users', function (req, res) {
+    DButilsAzure.executeQuery('SELECT * FROM Users')
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+});
 
-    // config for your database
-    var config = {
-        user: 'maorelf',
-        password: 'x5RwysYw',
-        server: 'assignment3webdev3maor.database.windows.net',
-        database: 'Ass3DB3',
-        options: {encrypt: true}
-
+app.post("/api/new_user", (req , res) => {
+    const user = {
+        username: req.body.username,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: req.body.password,
+        city: req.body.city,
+        country: req.body.country,
+        email: req.body.email,
+        questions_answers: req.body.questions_answers,
     };
-
-    // connect to your database
-    sql.close();
-    sql.connect(config, function (err) {
-
-        if (err)
+    console.log("got this user: " + req.body.first_name);
+    DButilsAzure.insertQuery('INSERT INTO Users (username,password,first_name,last_name,city,country,email,questions_answers) \n' +
+                                    'VALUES (@username,@password,@first_name,@last_name,@city,@country,@email,@questions_answers)',
+                                    user.username , user.password , user.first_name , user.last_name ,user.city , user.country , user.email ,user.questions_answers)
+        .then(function(result){
+            res.status(201).send(result)
+        })
+        .catch(function(err){
             console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-
-        // query to the database and get the records
-        request.query('select * from Users', function (err, recordset) {
-
-            if (err)
-                console.log(err)
-
-            // send records as a response
-            var w = recordset;
-            res.send(recordset);
-
-        });
-    });
+            res.send(err)
+        })
+    //res.status(201).send(username);
 });
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
 });
-
-
-function insert() {
-    return "select * from Users";
-}
-
 
 
 
