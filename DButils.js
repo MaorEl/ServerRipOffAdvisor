3319021895128c054b575055702a65fb55077760
AnaelGor <anaellegorfinkel@gmail.com>
2019-05-28 13:37:06 +0300

    get interest point details works

--- DButils.js
+++ DButils.js
@@ -88,289 +88,58 @@ exports.executeQuery = function (query) {
     });
 };
 
-exports.validateUser = function (query,username,question,answer) {
-    return new Promise(function (resolve, reject) {
-        try {
-            var ans = [];
-            var properties = [];
-            //acquire a connection
-            pool.acquire(function (err, connection) {
-                if (err) {
-                    console.log('acquire ' + err);
-                    reject(err);
-                }
-                console.log('connection on');
-
-                var dbReq = new Request(query, function (err, rowCount) {
-                    if (err) {
-                        console.log('Request ' + err);
-                        reject(err);
-                    }
-                });
-
-                dbReq.addParameter('username',TYPES.NVarChar,username);
-                dbReq.addParameter('question',TYPES.NVarChar,question);
-                dbReq.addParameter('answer',TYPES.NVarChar,answer);
-
-                dbReq.on('columnMetadata', function (columns) {
-                    columns.forEach(function (column) {
-                        if (column.colName != null)
-                            properties.push(column.colName);
-                    });
-                });
-                dbReq.on('row', function (row) {
-                    var item = {};
-                    for (i = 0; i < row.length; i++) {
-                        item[properties[i]] = row[i].value;
-                    }
-                    ans.push(item);
-                });
-
-                dbReq.on('requestCompleted', function () {
-                    console.log('request Completed: ' + dbReq.rowCount + ' row(s) returned');
-                    console.log(ans);
-                    connection.release();
-                    resolve(ans);
-
-                });
-                connection.execSql(dbReq);
-            });
-        }
-        catch (err) {
-            reject(err)
-        }
-    });
-};
-
-exports.getPassword = function (query,username) {
-    return new Promise(function (resolve, reject) {
-        try {
-            var ans = [];
-            var properties = [];
-            //acquire a connection
-            pool.acquire(function (err, connection) {
-                if (err) {
-                    console.log('acquire ' + err);
-                    reject(err);
-                }
-                console.log('connection on');
-
-                var dbReq = new Request(query, function (err, rowCount) {
-                    if (err) {
-                        console.log('Request ' + err);
-                        reject(err);
-                    }
-                });
-
-                dbReq.addParameter('username',TYPES.NVarChar,username);
-
-                dbReq.on('columnMetadata', function (columns) {
-                    columns.forEach(function (column) {
-                        if (column.colName != null)
-                            properties.push(column.colName);
-                    });
-                });
-                dbReq.on('row', function (row) {
-                    var item = {};
-                    for (i = 0; i < row.length; i++) {
-                        item[properties[i]] = row[i].value;
-                    }
-                    ans.push(item);
-                });
-
-                dbReq.on('requestCompleted', function () {
-                    console.log('request Completed: ' + dbReq.rowCount + ' row(s) returned');
-                    console.log(ans);
-                    connection.release();
-                    resolve(ans);
-
-                });
-                connection.execSql(dbReq);
-            });
-        }
-        catch (err) {
-            reject(err)
-        }
-    });
-};
-
-exports.insertUser = function (query,username,password,first_name,last_name,city,country,email) {
-    return new Promise(function (resolve, reject) {
-
-        try {
-
-            var ans = [];
-            var properties = [];
-
-            //acquire a connection
-            pool.acquire(function (err, connection) {
-                if (err) {
-                    console.log('acquire ' + err);
-                    reject(err);
-                }
-                console.log('connection on');
-
-                var dbReq = new Request(query, function (err, rowCount) {
-                    if (err) {
-                        console.log('Request ' + err);
-                        reject(err);
-                    }
-                });
-
-                dbReq.addParameter('username',TYPES.NVarChar,username);
-                dbReq.addParameter('password',TYPES.NVarChar,password);
-                dbReq.addParameter('first_name',TYPES.NVarChar,first_name);
-                dbReq.addParameter('last_name',TYPES.NVarChar,last_name);
-                dbReq.addParameter('city',TYPES.NVarChar,city);
-                dbReq.addParameter('country',TYPES.NVarChar,country);
-                dbReq.addParameter('email',TYPES.NVarChar,email);
+//
+// exports.getUser = function (query,username,password) {
+//     return new Promise(function (resolve, reject) {
+//         try {
+//             var ans = [];
+//             var properties = [];
+//             //acquire a connection
+//             pool.acquire(function (err, connection) {
+//                 if (err) {
+//                     console.log('acquire ' + err);
+//                     reject(err);
+//                 }
+//                 console.log('connection on');
+//
+//                 var dbReq = new Request(query, function (err, rowCount) {
+//                     if (err) {
+//                         console.log('Request ' + err);
+//                         reject(err);
+//                     }
+//                 });
+//
+//                 dbReq.addParameter('username',TYPES.NVarChar,username);
+//                 dbReq.addParameter('password',TYPES.NVarChar,password);
+//
+//                 dbReq.on('columnMetadata', function (columns) {
+//                     columns.forEach(function (column) {
+//                         if (column.colName != null)
+//                             properties.push(column.colName);
+//                     });
+//                 });
+//                 dbReq.on('row', function (row) {
+//                     var item = {};
+//                     for (i = 0; i < row.length; i++) {
+//                         item[properties[i]] = row[i].value;
+//                     }
+//                     ans.push(item);
+//                 });
+//
+//                 dbReq.on('requestCompleted', function () {
+//                     console.log('request Completed: ' + dbReq.rowCount + ' row(s) returned');
+//                     console.log(ans);
+//                     connection.release();
+//                     resolve(ans);
+//
+//                 });
+//                 connection.execSql(dbReq);
+//             });
+//         }
+//         catch (err) {
+//             reject(err)
+//         }
+//     });
+// };
 
 
-                dbReq.on('columnMetadata', function (columns) {
-                    columns.forEach(function (column) {
-                        if (column.colName != null)
-                            properties.push(column.colName);
-                    });
-                });
-                dbReq.on('row', function (row) {
-                    var item = {};
-                    for (i = 0; i < row.length; i++) {
-                        item[properties[i]] = row[i].value;
-                    }
-                    ans.push(item);
-                });
-
-                dbReq.on('requestCompleted', function () {
-                    console.log('request Completed: ' + dbReq.rowCount + ' row(s) returned');
-                    console.log(ans);
-                    connection.release();
-                    resolve(ans);
-
-                });
-                connection.execSql(dbReq);
-
-            });
-        }
-        catch (err) {
-            reject(err)
-        }
-    });
-
-};
-
-
-exports.getUser = function (query,username,password) {
-    return new Promise(function (resolve, reject) {
-        try {
-            var ans = [];
-            var properties = [];
-            //acquire a connection
-            pool.acquire(function (err, connection) {
-                if (err) {
-                    console.log('acquire ' + err);
-                    reject(err);
-                }
-                console.log('connection on');
-
-                var dbReq = new Request(query, function (err, rowCount) {
-                    if (err) {
-                        console.log('Request ' + err);
-                        reject(err);
-                    }
-                });
-
-                dbReq.addParameter('username',TYPES.NVarChar,username);
-                dbReq.addParameter('password',TYPES.NVarChar,password);
-
-                dbReq.on('columnMetadata', function (columns) {
-                    columns.forEach(function (column) {
-                        if (column.colName != null)
-                            properties.push(column.colName);
-                    });
-                });
-                dbReq.on('row', function (row) {
-                    var item = {};
-                    for (i = 0; i < row.length; i++) {
-                        item[properties[i]] = row[i].value;
-                    }
-                    ans.push(item);
-                });
-
-                dbReq.on('requestCompleted', function () {
-                    console.log('request Completed: ' + dbReq.rowCount + ' row(s) returned');
-                    console.log(ans);
-                    connection.release();
-                    resolve(ans);
-
-                });
-                connection.execSql(dbReq);
-            });
-        }
-        catch (err) {
-            reject(err)
-        }
-    });
-};
-
-
-
-exports.InsertAnswer = function (query,username,question,answer) {
-    return new Promise(function (resolve, reject) {
-
-        try {
-
-            var ans = [];
-            var properties = [];
-
-            //acquire a connection
-            pool.acquire(function (err, connection) {
-                if (err) {
-                    console.log('acquire ' + err);
-                    reject(err);
-                }
-                console.log('connection on');
-
-                var dbReq = new Request(query, function (err, rowCount) {
-                    if (err) {
-                        console.log('Request ' + err);
-                        reject(err);
-                    }
-                });
-
-                dbReq.addParameter('username',TYPES.NVarChar,username);
-                dbReq.addParameter('question',TYPES.Int,question);
-                dbReq.addParameter('answer',TYPES.VarChar,answer);
-
-
-
-                dbReq.on('columnMetadata', function (columns) {
-                    columns.forEach(function (column) {
-                        if (column.colName != null)
-                            properties.push(column.colName);
-                    });
-                });
-                dbReq.on('row', function (row) {
-                    var item = {};
-                    for (i = 0; i < row.length; i++) {
-                        item[properties[i]] = row[i].value;
-                    }
-                    ans.push(item);
-                });
-
-                dbReq.on('requestCompleted', function () {
-                    console.log('request Completed: ' + dbReq.rowCount + ' row(s) returned');
-                    console.log(ans);
-                    connection.release();
-                    resolve(ans);
-
-                });
-                connection.execSql(dbReq);
-
-            });
-        }
-        catch (err) {
-            reject(err)
-        }
-    });
-
-};
