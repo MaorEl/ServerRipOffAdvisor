365caeaa1dc02a62111260119bc54d861545cc2f
Maor Elfassy <maorelf@post.bgu.ac.il>
2019-05-30 20:07:56 +0300

    module seperation - now works

--- interestPoints.js
+++ interestPoints.js
@@ -317,7 +317,7 @@ function getTopFivePhotos (req, res) {
     exports.rankInterestPoint = rankInterestPoint;
     exports.deleteFromFavorites = deleteFromFavorites;
     exports.updateSortOption = updateSortOption;
-    exports.getAllCategories = getAllInterestPointsByCategory;
+    exports.getAllCategories = getAllCategories();
     exports.getAllFavorites = getAllFavorites;
     exports.getThreeRandom = getThreeRandom;
     exports.searchForInterestPoint  = searchForInterestPoint;
